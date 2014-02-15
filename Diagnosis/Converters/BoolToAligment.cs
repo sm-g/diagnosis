4c1325a21cbe14089a359767110673ba9f13df22
smg <semagerasimov@gmail.com>
2014-02-15 14:07:06 +0700

    tile item style

---
+++ Diagnosis/Converters/BoolToAligment.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+using System.Windows;
+
+namespace Diagnosis.Converters
+{
+    public class BoolToAligmentConverter<T> : IValueConverter
+    {
+        public BoolToAligmentConverter(T trueValue, T falseValue)
+        {
+            True = trueValue;
+            False = falseValue;
+        }
+
+        public T True { get; set; }
+        public T False { get; set; }
+
+        public virtual object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool && ((bool)value) ? True : False;
+        }
+
+        public virtual object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is T && EqualityComparer<T>.Default.Equals((T)value, True);
+        }
+    }
+
+    public sealed class BoolToCenterAligmentConverter : BoolToAligmentConverter<HorizontalAlignment>
+    {
+        public BoolToCenterAligmentConverter() :
+            base(HorizontalAlignment.Center, HorizontalAlignment.Left) { }
+    }
+}
