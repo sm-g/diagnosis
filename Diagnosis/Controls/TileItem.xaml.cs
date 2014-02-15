0188ffc0fd79a445716190b4b0d22974dfb2a123
smg <semagerasimov@gmail.com>
2014-02-15 15:15:44 +0700

    add editor in tile item

--- Diagnosis/Controls/TileItem.xaml.cs
+++ Diagnosis/Controls/TileItem.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Diagnosis.ViewModels;
 
 namespace Diagnosis.Controls
 {
@@ -22,6 +23,63 @@ namespace Diagnosis.Controls
         public TileItem()
         {
             InitializeComponent();
+        }
+
+        internal void BeginEdit()
+        {
+            ShowEditor();
+            titleEditor.Focus();
+        }
+
+        internal void CommitChanges()
+        {
+            HideEditor();
+        }
+
+        internal void RevertChanges()
+        {
+            titleEditor.Text = title.Text;
+            HideEditor();
+        }
+
+        private void ShowEditor()
+        {
+            editor.Visibility = System.Windows.Visibility.Visible;
+        }
+
+        private void HideEditor()
+        {
+            editor.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void editor_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var element = FocusManager.GetFocusedElement(Application.Current.MainWindow);
+
+            if (editor.Children.IndexOf((UIElement)element) == -1)
+            {
+                CommitChanges();
+            }
+        }
+
+        private void UserControl_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Space)
+            {
+                (DataContext as SymptomViewModel).ToggleChecked();
+            }
+            if (e.Key == Key.F2)
+            {
+                BeginEdit();
+            }
+            if (e.Key == Key.Enter)
+            {
+                CommitChanges();
+            }
+            if (e.Key == Key.Escape)
+            {
+                RevertChanges();
+            }
         }
     }
 }
