7baebff245c51f54e8b2f75efc54b5efe7adb6d6
smg <semagerasimov@gmail.com>
2014-02-15 09:33:29 +0700

    event in GroupsTree

--- Diagnosis/Controls/GroupsTree.xaml.cs
+++ Diagnosis/Controls/GroupsTree.xaml.cs
@@ -1,16 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Diagnosis.Helpers;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Diagnosis.Controls
 {
@@ -25,8 +16,62 @@ namespace Diagnosis.Controls
         }
         private void TreeView_Loaded(object sender, RoutedEventArgs e)
         {
-            DataContext = Diagnosis.DataCreator.CreateSymptoms();
+            tree.DataContext = Diagnosis.DataCreator.CreateSymptoms();
 
+        }
+
+        private TreeItem FindTreeItem(object sender)
+        {
+            return ChildFinder.FindChild<TreeItem>((DependencyObject)sender, "treeItem");
+        }
+
+        private void item_LostFocus(object sender, RoutedEventArgs e)
+        {
+            e.Handled = true;
+            var q = FindTreeItem(sender);
+
+            //q.CommitChanges();
+        }
+
+        private void item_KeyUp(object sender, KeyEventArgs e)
+        {
+            e.Handled = true;
+            var q = FindTreeItem(sender);
+            if (e.Key == Key.Space)
+            {
+                q.ToggleCheckedState();
+            }
+            if (e.Key == Key.F2)
+            {
+                q.BeginEdit();
+            }
+            if (e.Key == Key.Enter)
+            {
+                q.CommitChanges();
+            }
+            if (e.Key == Key.Escape)
+            {
+                q.RevertChanges();
+            }
+            if (e.Key == Key.Insert || e.Key == Key.F && ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control))
+            {
+                q.BeginSearch();
+            }
+        }
+
+        private void item_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            var q = FindTreeItem(sender);
+            q.ToggleCheckedState();
+        }
+
+        private void item_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            var q = FindTreeItem(sender);
+            if ((sender as TreeViewItem).IsSelected) // to prevent multiplay raising http://stackoverflow.com/questions/2280049
+                q.BeginEdit();
         }
     }
 }
