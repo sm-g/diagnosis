b5272e1f673fbac9a67e3f2eb21ca232d8504ebd
smg <semagerasimov@gmail.com>
2014-02-17 09:49:37 +0700

    add search in TileView

--- Diagnosis/Controls/TileView.xaml.cs
+++ Diagnosis/Controls/TileView.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System.Windows;
+﻿using Diagnosis.ViewModels;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Diagnosis.Controls
 {
@@ -13,9 +15,52 @@ namespace Diagnosis.Controls
             InitializeComponent();
         }
 
+        internal void BeginSearch()
+        {
+            search.DataContext = new SearchViewModel((explorer.DataContext as SymptomExplorerViewModel).CurrentSymptom);
+            search.Visibility = System.Windows.Visibility.Visible;
+            search.Focus();
+        }
+
         private void explorer_Loaded(object sender, RoutedEventArgs e)
         {
             explorer.DataContext = new Diagnosis.ViewModels.SymptomExplorerViewModel(Diagnosis.DataCreator.CreateSymptoms());
+        }
+
+        private void EndSearch()
+        {
+            search.Visibility = System.Windows.Visibility.Collapsed;
+        }
+
+        private void search_KeyUp(object sender, KeyEventArgs e)
+        {
+            var searchVM = search.DataContext as SearchViewModel;
+            var symptomVM = (explorer.DataContext as SymptomExplorerViewModel).CurrentSymptom;
+
+            if (e.Key == Key.Escape)
+            {
+                EndSearch();
+            }
+            if (e.Key == Key.Enter)
+            {
+                if (symptomVM.Children.IndexOf(searchVM.SelectedItem) == -1)
+                {
+                    symptomVM.Add(searchVM.SelectedItem);
+                }
+
+                if (searchVM.SelectedItem != null)
+                    searchVM.SelectedItem.ToggleChecked();
+
+                searchVM.Clear();
+            }
+        }
+
+        private void explorer_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Insert || e.Key == Key.F && ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control))
+            {
+                BeginSearch();
+            }
         }
     }
 }
