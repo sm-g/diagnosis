be5415c9aae7a75b0f5dcbf37f909178b1985608
smg <semagerasimov@gmail.com>
2014-03-01 22:20:55 +0700

    add AutoComplete

---
+++ Diagnosis/Controls/AutoComplete.xaml.cs
@@ -0,0 +1,122 @@
+﻿using Diagnosis.ViewModels;
+using System.Runtime.InteropServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Collections.Generic;
+
+namespace Diagnosis.Controls
+{
+    /// <summary>
+    /// Interaction logic for AutoComplete.xaml
+    /// </summary>
+    public partial class AutoComplete : UserControl
+    {
+        AutoCompleteViewModel vm
+        {
+            get
+            {
+                return (DataContext ?? (DataContext = new AutoCompleteViewModel())) as AutoCompleteViewModel;
+            }
+        }
+
+        public AutoComplete()
+        {
+            InitializeComponent();
+
+            DataContext = new AutoCompleteViewModel();
+        }
+
+        private void input_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            ShowSuggestionsPopup();
+        }
+
+        private void input_PreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Up && suggestions.SelectedIndex > 0)
+            {
+                suggestions.SelectedIndex--;
+            }
+            if (e.Key == Key.Down && suggestions.SelectedIndex < suggestions.Items.Count - 1)
+            {
+                suggestions.SelectedIndex++;
+            }
+        }
+
+        private void suggestions_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    AddSuggestion();
+                    break;
+
+                case Key.Escape:
+                    input.Focus();
+                    break;
+            }
+        }
+        private void input_KeyDown(object sender, KeyEventArgs e)
+        {
+        }
+
+        private void input_KeyUp(object sender, KeyEventArgs e)
+        {
+            switch (e.Key)
+            {
+                case Key.Enter:
+                    if (vm.IsSymptomCompleted)
+                    {
+                        vm.Clear();
+                    }
+                    else
+                    {
+                        AddSuggestion();
+                    }
+                    break;
+            }
+        }
+
+        private void ShowSuggestionsPopup()
+        {
+            Rect placementRect;
+            if (input.CaretIndex != 0)
+            {
+                placementRect = input.GetRectFromCharacterIndex(input.CaretIndex, true);
+            }
+            else
+            {
+                placementRect = new Rect(1, 1, 0, input.ActualHeight);
+            }
+            popup.PlacementTarget = input;
+            popup.PlacementRectangle = placementRect;
+
+            popup.IsOpen = suggestions.Items.Count > 0;
+        }
+
+
+        private void AddSuggestion()
+        {
+            vm.Accept();
+
+            input.CaretIndex = input.Text.Length;
+        }
+
+        private void input_GotFocus(object sender, RoutedEventArgs e)
+        {
+            ShowSuggestionsPopup();
+        }
+
+        private void input_GotMouseCapture(object sender, MouseEventArgs e)
+        {
+            ShowSuggestionsPopup();
+        }
+
+        private void DockPanel_MouseUp(object sender, MouseButtonEventArgs e)
+        {
+            AddSuggestion();
+            input.Focus();
+        }
+    }
+}
---
+++ Diagnosis/ViewModels/AutoCompleteViewModel.cs
@@ -0,0 +1,197 @@
+﻿using Diagnosis.Models;
+using EventAggregator;
+using System.Collections.ObjectModel;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Diagnosis.ViewModels
+{
+    public class AutoCompleteViewModel : ViewModelBase
+    {
+        List<SymptomViewModel> symptoms;
+        SearchViewModel search;
+
+        private string _fullSymptom = "";
+        private bool _isSymptomCompleted;
+        int _selectedIndex;
+        const char SymptomSeparator = ',';
+
+        public string FullSymptom
+        {
+            get
+            {
+                return _fullSymptom;
+            }
+            set
+            {
+                if (_fullSymptom != value)
+                {
+                    CheckCompleted(value);
+
+                    _fullSymptom = value;
+
+                    MakeSuggestions();
+
+                    OnPropertyChanged(() => FullSymptom);
+                }
+            }
+        }
+
+        public string SymptomsChainString
+        {
+            get
+            {
+                return symptoms.Aggregate("", (full, s) => full += s.Name.ToLower() + SymptomSeparator).TrimEnd(SymptomSeparator);
+            }
+        }
+
+        public string LastPart
+        {
+            get
+            {
+                return FullSymptom.Substring(SymptomsChainString.Length).Trim(SymptomSeparator);
+            }
+        }
+
+        bool IsCompleted()
+        {
+            return !IsSymptomCompleted && search.SelectedItem != null && LastPart == search.SelectedItem.Name;
+        }
+
+        /// <summary>
+        /// Проверка завершенности симптома в строке
+        /// </summary>
+        /// <param name="value"></param>
+        private void CheckCompleted(string value)
+        {
+            if (value.Length < FullSymptom.Length)
+            {
+                // символ удалён
+
+                if (IsSymptomCompleted)
+                {
+                    // удаляем последний символ симптома
+                    symptoms.RemoveAt(symptoms.Count - 1);
+                    SetSearchContext(true);
+                }
+                else if (FullSymptom[FullSymptom.Length - 1] == SymptomSeparator)
+                {
+                    // удаляем разделитель симптомов
+                    IsSymptomCompleted = true;
+                    SetSearchContext(false);
+                }
+            }
+            else if (value[value.Length - 1] == SymptomSeparator)
+            {
+                // добавляем разделитель
+
+                if (IsSymptomCompleted)
+                {
+                    SetSearchContext(true);
+                }
+                else
+                {
+                    AddSymptom();
+                    SetSearchContext(true);
+                }
+            }
+        }
+
+        private void MakeSuggestions()
+        {
+            if (!IsSymptomCompleted)
+            {
+                search.Query = LastPart;
+            }
+            else
+            {
+                search.Query = symptoms[symptoms.Count - 1].Name;
+            }
+            Suggestions = search.Results;
+            OnPropertyChanged(() => Suggestions);
+            SelectedIndex = 0;
+
+        }
+        public ObservableCollection<SymptomViewModel> Suggestions { get; private set; }
+
+        public int SelectedIndex
+        {
+            get
+            {
+                return _selectedIndex;
+            }
+            set
+            {
+                if (_selectedIndex != value)
+                {
+                    _selectedIndex = value;
+                    search.SelectedIndex = value;
+                    OnPropertyChanged(() => SelectedIndex);
+                }
+            }
+        }
+
+        public bool IsSymptomCompleted
+        {
+            get
+            {
+                return _isSymptomCompleted;
+            }
+            set
+            {
+                if (_isSymptomCompleted != value)
+                {
+                    _isSymptomCompleted = value;
+                    OnPropertyChanged(() => IsSymptomCompleted);
+                }
+            }
+        }
+
+        public void Accept()
+        {
+            AddSymptom();
+            FullSymptom = SymptomsChainString;
+        }
+        public void Clear()
+        {
+            symptoms.Clear();
+            SetSearchContext(true);
+            FullSymptom = "";
+        }
+
+        void AddSymptom()
+        {
+            symptoms.Add(search.SelectedItem);
+            IsSymptomCompleted = true;
+        }
+
+        void SetSearchContext(bool symptomStarted)
+        {
+            var i = symptoms.Count - 1;
+            if (!symptomStarted)
+            {
+                i--;
+            }
+            if (i < 0)
+                search = new SearchViewModel(DataCreator.CreateSymptoms()[0]);
+            else
+                search = new SearchViewModel(symptoms[i]);
+            search.WithGroups = true;
+
+            if (symptomStarted)
+            {
+                IsSymptomCompleted = false;
+            }
+        }
+
+        public AutoCompleteViewModel()
+        {
+            symptoms = new List<SymptomViewModel>();
+
+            SetSearchContext(true);
+            MakeSuggestions();
+        }
+
+    }
+}
