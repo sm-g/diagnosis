8186c7cdf95641c4ffdcfc1e436f5ba2f25a3184
smg <semagerasimov@gmail.com>
2014-03-09 11:59:24 +0700

    add diagnoses data

--- Diagnosis/DataCreator.cs
+++ Diagnosis/DataCreator.cs
@@ -66,22 +66,52 @@ namespace Diagnosis
             var teeth = new SymptomViewModel("зубная боль");
             teeth.Add(new SymptomViewModel("после приёма пищи")).Add(new SymptomViewModel("шатается зуб"));
 
-            var throat = new SymptomViewModel(new Symptom("горло")) { IsNonCheckable = true };
+            var throat = new SymptomViewModel("горло") { IsNonCheckable = true };
             throat.Add(new SymptomViewModel("боль в горле")
                     .Add(new SymptomViewModel("при глотании"))
                     .Add(new SymptomViewModel("после разговора")))
                 .Add(new SymptomViewModel("красное"));
 
-            SymptomViewModel root = (new SymptomViewModel(new Symptom("root")).Add(
-                    (new SymptomViewModel("голова") { IsNonCheckable = true }).Add(throat)
+            SymptomViewModel root = (new SymptomViewModel("root") { IsNonCheckable = true })
+                .Add((new SymptomViewModel("голова") { IsNonCheckable = true }).Add(throat)
                         .Add(teeth)
                         .Add(new SymptomViewModel("уши"))
-                ).Add(
-                new SymptomViewModel("ноги") { IsNonCheckable = true }));
+                ).Add(new SymptomViewModel("ноги") { IsNonCheckable = true });
 
             root.Initialize();
 
             return new List<SymptomViewModel>(root.Children);
+        }
+
+        private static List<DiagnosisViewModel> _Diagnoses;
+
+        public static List<DiagnosisViewModel> Diagnoses
+        {
+            get
+            {
+                return _Diagnoses ?? (_Diagnoses = CreateDiagnoses());
+            }
+        }
+
+        private static List<DiagnosisViewModel> CreateDiagnoses()
+        {
+            DiagnosisViewModel root = new DiagnosisViewModel("root")
+                .Add(new DiagnosisViewModel("Некоторые инфекционные и паразитарные болезни")
+                    .Add(new DiagnosisViewModel("Кишечные инфекции")
+                        .Add(new DiagnosisViewModel("Холера")
+                            .Add(new DiagnosisViewModel("Холера, вызванная вибрионом 01, биовар cholerae"))
+                            .Add(new DiagnosisViewModel("Холера, вызванная вибрионом 01, биовар cholerae")))
+                        .Add(new DiagnosisViewModel("Другие протозойные кишечные болезни")
+                            .Add(new DiagnosisViewModel("Балантидиаз"))
+                            .Add(new DiagnosisViewModel("Жиардиаз (лямблиоз)"))
+                            .Add(new DiagnosisViewModel("Протозойная кишечная болезнь неуточненная")))
+                        )
+                    )
+                ;
+
+            root.Initialize();
+
+            return new List<DiagnosisViewModel>(root.Children);
         }
     }
 }
