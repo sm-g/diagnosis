45f19047229e3b69fa20d91ae3caca1c93cd5d18
smg <semagerasimov@gmail.com>
2014-03-10 12:06:43 +0700

    extended ISearchable

--- Diagnosis/ViewModels/ISearchable.cs
+++ Diagnosis/ViewModels/ISearchable.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 using System.Text;
 
 namespace Diagnosis.ViewModels
@@ -8,5 +9,8 @@ namespace Diagnosis.ViewModels
     public interface ISearchable
     {
         string Representation { get; }
+        ICommand SearchCommand { get; }
+        bool IsSearchActive { get; set; }
+        bool IsSearchFocused { get; set; }
     }
 }
