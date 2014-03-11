dac73af7bd2f3d051b41205647e577de7cfcf38d
smg <semagerasimov@gmail.com>
2014-03-11 22:16:18 +0700

    add Cource, Appointment and HealthRecord models

---
+++ Diagnosis/Models/Appointment.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diagnosis.Models
+{
+    public class Appointment
+    {
+        public Course Course { get; set; }
+        public Doctor Doctor { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
---
+++ Diagnosis/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diagnosis.Models
+{
+    public class Course
+    {
+        public Patient Patient { get; set; }
+        public Doctor LeadDoctor { get; set; }
+        public DateTime Start { get; set; }
+        public DateTime End { get; set; }
+    }
+}
