4902c57e8c3303a3ee087edce0eb7c940834a677
smg <semagerasimov@gmail.com>
2014-03-19 15:28:53 +0700

    remove unnecessary repositories

--- Diagnosis.Data/Repositories/IModelRepositories.cs
+++ Diagnosis.Data/Repositories/IModelRepositories.cs
@@ -7,34 +7,22 @@ namespace Diagnosis.Data.Repositories
     {
         Patient GetByName(string name);
     }
+
     public interface IPropertyRepository : IRepository<Property>
     {
         Property GetByTitle(string title);
     }
-    public interface IPropertyValueRepository : IRepository<PropertyValue>
-    {
-        PropertyValue GetByValue(string value);
-    }
-    public interface IPatientPropertyRepository : IRepository<PatientProperty>
-    {
-    }
+
     public interface IDoctorRepository : IRepository<Doctor>
     {
         Doctor GetByName(string name);
     }
-    public interface ICourseRepository : IRepository<Course>
-    {
-    }
-    public interface IAppointmentRepository : IRepository<Appointment>
-    {
-    }
-    public interface IHealthRecordRepository : IRepository<HealthRecord>
-    {
-    }
+
     public interface ISymptomRepository : IRepository<Symptom>
     {
         Symptom GetByTitle(string title);
     }
+
     public interface IDiagnosisRepository : IRepository<Diagnosis.Models.Diagnosis>
     {
         Diagnosis.Models.Diagnosis GetByTitle(string title);
