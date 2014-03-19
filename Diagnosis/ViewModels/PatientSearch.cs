25677ddb7eb9cdba1e3b3f42fb430432eee545c7
smg <semagerasimov@gmail.com>
2014-03-19 22:32:25 +0700

    create valid Patient from search query

--- Diagnosis/ViewModels/PatientSearch.cs
+++ Diagnosis/ViewModels/PatientSearch.cs
@@ -1,4 +1,5 @@
 ﻿using Diagnosis.Models;
+using Diagnosis.Data.Repositories;
 using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics.Contracts;
@@ -8,11 +9,14 @@ namespace Diagnosis.ViewModels
 {
     public class PatientSearch : SearchBase<PatientViewModel>
     {
-        public PatientSearch(PatientsListVewModel patients)
+        IPropertyManager propManager;
+
+        public PatientSearch(PatientsListVewModel patients, IPropertyManager propManager)
             : base()
         {
             Contract.Requires(patients != null);
             Collection = patients.Patients;
+            this.propManager = propManager;
 
             InitQuery();
         }
@@ -21,16 +25,14 @@ namespace Diagnosis.ViewModels
         {
             var lfmNames = query.Split().ToList();
 
-            while (lfmNames.Count < 3)
+            // no patient without first and last names
+            if (lfmNames.Count < 2 || lfmNames.Any(s => String.IsNullOrWhiteSpace(s)))
             {
-                lfmNames.Add("");
+                return null;
             }
-            return new PatientViewModel(new Patient()
-                 {
-                     LastName = lfmNames[0],
-                     FirstName = lfmNames[1],
-                     MiddleName = lfmNames[2],
-                 });
+
+            string middleName = lfmNames.Count < 3 ? null : lfmNames[2];
+            return new PatientViewModel(new Patient(lfmNames[0], lfmNames[1], new DateTime(1980, 6, 15), middleName), propManager);
         }
     }
 }
