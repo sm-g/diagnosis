debda51d0d605595fcc1397641f56c46171ad943
smg <semagerasimov@gmail.com>
2014-03-19 22:30:25 +0700

    add PropertyManager

---
+++ Diagnosis/ViewModels/IPropertyManager.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using Diagnosis.Models;
+
+namespace Diagnosis.ViewModels
+{
+    public interface IPropertyManager
+    {
+        List<PropertyViewModel> GetPatientProperties(Patient patient);
+    }
+}
---
+++ Diagnosis/ViewModels/PropertyManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using Diagnosis.Data.Repositories;
+using Diagnosis.Models;
+using System.Diagnostics.Contracts;
+
+namespace Diagnosis.ViewModels
+{
+    public class PropertyManager : IPropertyManager
+    {
+        IPropertyRepository propRepo;
+
+        public List<PropertyViewModel> GetPatientProperties(Patient patient)
+        {
+            Contract.Requires(patient != null);
+
+            var allProperties = propRepo.GetAll();
+
+            var existingPatProps = patient.PatientProperties;
+            var properties = new List<PropertyViewModel>(allProperties.Select(p => new PropertyViewModel(p)));
+
+            // указываем значение свойства из БД
+            // если у пацента не указано какое-то свойство — добавляем это свойство с пустым значением
+            foreach (var propVM in properties)
+            {
+                var pp = existingPatProps.FirstOrDefault(patProp => patProp.Property.Id == propVM.PropertyId);
+                if (pp != null)
+                {
+                    propVM.SelectedValue = pp.Value;
+                }
+                else
+                {
+                    propVM.SelectedValue = new EmptyPropertyValue(propRepo.GetById(propVM.PropertyId));
+                }
+            }
+
+            return properties;
+        }
+
+        public PropertyManager(IPropertyRepository propRepository)
+        {
+            Contract.Requires(propRepository != null);
+            propRepo = propRepository;
+        }
+    }
+}
