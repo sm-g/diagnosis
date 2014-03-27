98fa7f9d37b32475e46e5ea5ff021393b5c2ab44
smg <semagerasimov@gmail.com>
2014-03-27 22:37:29 +0700

    fix CountToVisibility name

--- Diagnosis.App/Converters/CountToVisibility.cs
+++ Diagnosis.App/Converters/CountToVisibility.cs
@@ -1,13 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Windows.Data;
 using System.Globalization;
+using System.Windows.Data;
 
 namespace Diagnosis.App.Converters
 {
-    public class CountToVisability : IValueConverter
+    public class CountToVisibility : IValueConverter
     {
         public object Convert(object value, Type targetType,
             object parameter, CultureInfo culture)
