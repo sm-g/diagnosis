852c4d78514d275ca7db9edf75406d268d1eeb29
smg <semagerasimov@gmail.com>
2014-04-06 19:52:57 +0700

    add TreeItemLight without search and editor for speed up loading tree

---
+++ Diagnosis.App/Controls/Directory/TreeItemLight.xaml.cs
@@ -0,0 +1,17 @@
+﻿
+using Diagnosis.App.ViewModels;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Diagnosis.App.Controls
+{
+    public partial class TreeItemLight : UserControl
+    {
+        public TreeItemLight()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Diagnosis.App/Controls/Directory/TreeTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System;
+using System.Collections.Generic;
+
+namespace Diagnosis.App.Controls
+{
+    class TreeTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate FullTreeWithSearch { get; set; }
+        public DataTemplate FullTreeLight { get; set; }
+
+        public override DataTemplate SelectTemplate(object item, DependencyObject container)
+        {
+            return null;
+        }
+    }
+}
