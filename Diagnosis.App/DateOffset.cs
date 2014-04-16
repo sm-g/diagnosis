f9c8de49f7217081447519471226b35f1b0102e7
smg <semagerasimov@gmail.com>
2014-04-16 17:51:59 +0700

    DateOffseet class with tests

---
+++ Diagnosis.App/DateOffset.cs
@@ -0,0 +1,286 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+
+namespace Diagnosis.App
+{
+    public enum DateUnits
+    {
+        Day,
+        Month,
+        Year
+    }
+}
+
+namespace Diagnosis.App.ViewModels
+{
+    /// <summary>
+    /// Неполная дата со смещением относительно сегодня в днях, месяцах или годах, зависит от полноты даты.
+    /// </summary>
+    public class DateOffset : ViewModelBase
+    {
+        public const int MinYear = 1880;
+
+        private int _offset;
+        private DateUnits _unit;
+        private int? _year;
+        private int? _month;
+        private int? _day;
+
+        bool setting;
+
+        readonly public Func<DateTime> NowDate = () => DateTime.Today;
+
+        public int? Year
+        {
+            get
+            {
+                return _year;
+            }
+            set
+            {
+                if (_year != value)
+                {
+                    _year = value;
+
+                    if (!setting)
+                    {
+                        SetDate(value, Month, Day);
+                    }
+                    OnPropertyChanged(() => Year);
+                }
+            }
+        }
+        public int? Month
+        {
+            get
+            {
+                return _month;
+            }
+            set
+            {
+                if (_month != value)
+                {
+                    _month = value;
+                    if (!setting)
+                    {
+                        SetDate(Year, value, Day);
+                    }
+                    OnPropertyChanged(() => Month);
+                }
+            }
+        }
+        public int? Day
+        {
+            get
+            {
+                return _day;
+            }
+            set
+            {
+                if (_day != value)
+                {
+                    _day = value;
+                    if (!setting)
+                    {
+                        SetDate(Year, Month, value);
+                    }
+                    OnPropertyChanged(() => Day);
+                }
+            }
+        }
+
+        public int Offset
+        {
+            get
+            {
+                return _offset;
+            }
+            set
+            {
+                if (_offset != value)
+                {
+                    _offset = value;
+                    if (!setting)
+                    {
+                        SetOffset(value, Unit);
+                    }
+                    OnPropertyChanged(() => Unit);
+                    OnPropertyChanged(() => Offset);
+                }
+            }
+        }
+        public DateUnits Unit
+        {
+            get
+            {
+                return _unit;
+            }
+            set
+            {
+                if (_unit != value)
+                {
+                    _unit = value;
+                    if (!setting)
+                    {
+                        SetOffset(Offset, value);
+                    }
+                    OnPropertyChanged(() => Unit);
+
+                }
+            }
+        }
+
+        DateTime Now { get { return NowDate(); } }
+
+        public void SetOffset(int offset, DateUnits unit)
+        {
+            setting = true;
+
+            Offset = offset;
+            Unit = unit;
+
+            switch (Unit)
+            {
+                case DateUnits.Day:
+                    var date = Now.AddDays(-Offset);
+                    Year = date.Year;
+                    Month = date.Month;
+                    Day = date.Day;
+                    break;
+                case DateUnits.Month:
+                    if (Offset < Now.Month)
+                    {
+                        Year = Now.Year;
+                        Month = Now.Month - Offset;
+                    }
+                    else
+                    {
+                        var a = Offset - Now.Month;
+                        Year = Now.Year - a / 12 - 1;
+                        Month = 12 - a % 12;
+                    }
+                    Day = null;
+                    break;
+                case DateUnits.Year:
+                    Year = Now.Year - Offset;
+                    Month = null;
+                    Day = null;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("Unit");
+            }
+
+            setting = false;
+        }
+
+        public void SetDate(int? year, int? month, int? day)
+        {
+            if ((year.HasValue || month.HasValue || day.HasValue) == false)
+                throw new ArgumentNullException("Date cannot be empty.");
+
+            setting = true;
+
+            Year = year;
+            Month = month;
+            Day = day;
+
+            int y;
+            if (year.HasValue)
+            {
+                y = year.Value;
+            }
+            else
+            {
+                y = Now.Year;
+            }
+
+            if (month.HasValue)
+            {
+                if (day.HasValue)
+                {
+                    Offset = (Now - new DateTime(y, month.Value, day.Value)).Days;
+                    Unit = DateUnits.Day;
+                }
+                else
+                {
+                    Offset = (Now.Year - y) * 12 + Now.Month - month.Value;
+                    Unit = DateUnits.Month;
+                }
+
+                if (!year.HasValue)
+                {
+                    Year = Now.Year;
+                }
+            }
+            else
+            {
+                if (year.HasValue)
+                {
+                    Offset = Now.Year - y;
+                    Unit = DateUnits.Year;
+                }
+                else
+                {
+                    if (day.HasValue)
+                    {
+                        Offset = 0;
+                        Unit = DateUnits.Day;
+
+                        Month = Now.Month;
+                        Year = Now.Year;
+                    }
+
+                }
+            }
+
+            setting = false;
+        }
+        /// <summary>
+        /// Проверяет, допустима ли указанная дата (возможно, неполная).
+        /// <exception cref="ArgumentOutOfRange">Если дата невозможна.</exception>
+        /// </summary>
+        public static void CheckDate(int? year, int? month, int? day)
+        {
+            if (month.HasValue && day.HasValue)
+            {
+                if (year.HasValue)
+                {
+                    if (year < MinYear)
+                    {
+                        throw new ArgumentOutOfRangeException("year");
+                    }
+
+                    // пробуем создать дату
+                    new DateTime(year.Value, month.Value, day.Value);
+                }
+                else if (day.Value > DateTime.DaysInMonth(DateTime.Today.Year, month.Value))
+                {
+                    throw new ArgumentOutOfRangeException("day");
+                }
+            }
+        }
+
+        public DateOffset(int? year, int? month, int? day)
+        {
+            SetDate(year, month, day);
+        }
+
+        public DateOffset(int? year, int? month, int? day, Func<DateTime> now)
+        {
+            Contract.Requires(now != null);
+            NowDate = now;
+            SetDate(year, month, day);
+        }
+
+        public DateOffset(int offset, DateUnits unit)
+        {
+            SetOffset(offset, unit);
+        }
+        public DateOffset(int offset, DateUnits unit, Func<DateTime> now)
+        {
+            Contract.Requires(now != null);
+            NowDate = now;
+            SetOffset(offset, unit);
+        }
+    }
+}
---
+++ Tests/DateOffsetTest.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using Diagnosis.App.ViewModels;
+using Diagnosis.App;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Tests
+{
+    [TestClass]
+    public class DateOffsetTest
+    {
+        static DateOffset date;
+        static Func<DateTime> now = () => new DateTime(2014, 4, 1);
+
+        const int offset = 5;
+
+        #region constructors
+
+        [TestMethod]
+        public void TestConstructorOffsetDay()
+        {
+            date = new DateOffset(offset, DateUnits.Day);
+
+            var now = DateTime.Today;
+            var d = now.AddDays(-offset);
+
+            Assert.IsTrue(date.Offset == offset);
+            Assert.IsTrue(date.Unit == DateUnits.Day);
+            Assert.IsTrue(date.Year == d.Year);
+            Assert.IsTrue(date.Month == d.Month);
+            Assert.IsTrue(date.Day == d.Day);
+        }
+
+        [TestMethod]
+        public void TestConstructorOffsetMonth()
+        {
+            date = new DateOffset(offset, DateUnits.Month);
+
+            var now = DateTime.Today;
+            var d = now.AddMonths(-offset);
+
+            Assert.IsTrue(date.Offset == offset);
+            Assert.IsTrue(date.Unit == DateUnits.Month);
+            Assert.IsTrue(date.Year == d.Year);
+            Assert.IsTrue(date.Month == d.Month);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestConstructorOffsetYear()
+        {
+            date = new DateOffset(offset, DateUnits.Year);
+
+            var now = DateTime.Today;
+            var d = now.AddYears(-offset);
+
+            Assert.IsTrue(date.Offset == offset);
+            Assert.IsTrue(date.Unit == DateUnits.Year);
+            Assert.IsTrue(date.Year == d.Year);
+            Assert.IsTrue(date.Month == null);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateFull()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(now.Year, now.Month, now.Day);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Day);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == now.Month);
+            Assert.IsTrue(date.Day == now.Day);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateYear()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(now.Year, null, null);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Year);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == null);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateYearMonth()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(now.Year, now.Month, null);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Month);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == now.Month);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateYearDay()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(now.Year, null, now.Day);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Year);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == null);
+            Assert.IsTrue(date.Day == now.Day);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateMonth()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(null, now.Month, null);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Month);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == now.Month);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateMonthDay()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(null, now.Month, now.Day);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Day);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == now.Month);
+            Assert.IsTrue(date.Day == now.Day);
+        }
+
+        [TestMethod]
+        public void TestConstructorDateDay()
+        {
+            var now = DateTime.Today;
+
+            date = new DateOffset(null, null, now.Day);
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Day);
+            Assert.IsTrue(date.Year == now.Year);
+            Assert.IsTrue(date.Month == now.Month);
+            Assert.IsTrue(date.Day == now.Day);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestConstructorDateNone()
+        {
+            date = new DateOffset(null, null, null);
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void TestSetDay()
+        {
+            date = new DateOffset(offset, DateUnits.Day);
+            var now = DateTime.Today;
+            date.Day = now.Day;
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Day);
+        }
+
+        [TestMethod]
+        public void TestSetMonth()
+        {
+            date = new DateOffset(offset, DateUnits.Day);
+            var d = DateTime.Today.AddMonths(-1);
+            date.Month = d.Month;
+
+            Assert.IsTrue(date.Offset == DateTime.DaysInMonth(
+                DateTime.Today.Year, d.Month) + offset);
+            Assert.IsTrue(date.Unit == DateUnits.Day);
+        }
+
+        [TestMethod]
+        public void TestSetMonthNull()
+        {
+            date = new DateOffset(offset, DateUnits.Day);
+            date.Month = null;
+
+            Assert.IsTrue(date.Offset == 0);
+            Assert.IsTrue(date.Unit == DateUnits.Year);
+        }
+
+        [TestMethod]
+        public void TestSetUnitLess()
+        {
+            date = new DateOffset(offset, DateUnits.Day, now);
+
+            date.Unit = DateUnits.Month;
+
+            Assert.IsTrue(date.Offset == offset);
+            Assert.IsTrue(date.Unit == DateUnits.Month);
+            Assert.IsTrue(date.Year == 2013);
+            Assert.IsTrue(date.Month == 11);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestSetUnitGreater()
+        {
+            date = new DateOffset(offset, DateUnits.Year, now);
+
+            date.Unit = DateUnits.Month;
+
+            Assert.IsTrue(date.Offset == offset);
+            Assert.IsTrue(date.Unit == DateUnits.Month);
+            Assert.IsTrue(date.Year == 2013);
+            Assert.IsTrue(date.Month == 11);
+            Assert.IsTrue(date.Day == null);
+        }
+
+        [TestMethod]
+        public void TestSetMonth2()
+        {
+            date = new DateOffset(1, DateUnits.Year, now);
+
+            date.Month = 4;
+
+            Assert.IsTrue(date.Offset == 12);
+            Assert.IsTrue(date.Unit == DateUnits.Month);
+            Assert.IsTrue(date.Year == 2013);
+            Assert.IsTrue(date.Month == 4);
+            Assert.IsTrue(date.Day == null);
+        }
+    }
+}
