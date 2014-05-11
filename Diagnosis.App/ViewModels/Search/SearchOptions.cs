d05c742b477a20de79a037288e9ef04b9141989e
smg <semagerasimov@gmail.com>
2014-05-11 17:12:48 +0700

    add SearchOptions

---
+++ Diagnosis.App/ViewModels/Search/SearchOptions.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diagnosis.App.ViewModels
+{
+    public class SearchOptions
+    {
+        /// <summary>
+        /// Слова, которые есть в симптоме
+        /// </summary>
+        public IEnumerable<WordViewModel> Words { get; set; }
+        /// <summary>
+        /// Достаточно ли любого слова в симптоме
+        /// </summary>
+        public bool AnyWord { get; set; }
+        /// <summary>
+        /// Категория. Если несколько, то любая их них.
+        /// </summary>
+        public IEnumerable<CategoryViewModel> Categories { get; set; }
+        /// <summary>
+        /// Нижняя грань даты приема
+        /// </summary>
+        public DateOffset AppointmentDateGt { get; set; }
+        /// <summary>
+        /// Верхняя грань даты приема
+        /// </summary>
+        public DateOffset AppointmentDateLt { get; set; }
+        /// <summary>
+        /// Нижняя грань давности симптома
+        /// </summary>
+        public DateOffset HealthRecordFromDateGt { get; set; }
+        /// <summary>
+        /// Верхняя грань давности симптома
+        /// </summary>
+        public DateOffset HealthRecordFromDateLt { get; set; }
+    }
+}
