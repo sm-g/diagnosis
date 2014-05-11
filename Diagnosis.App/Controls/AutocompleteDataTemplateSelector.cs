081fdedb7d82a18fe0d65fee368b77bc025a5e94
smg <semagerasimov@gmail.com>
2014-05-11 17:12:03 +0700

    template selector in autocomplete suggestions list

---
+++ Diagnosis.App/Controls/AutocompleteDataTemplateSelector.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using Diagnosis.App.ViewModels;
+
+namespace Diagnosis.App.Controls
+{
+    public class AutocompleteDataTemplateSelector : DataTemplateSelector
+    {
+        public DataTemplate DiagnosisTemplate { get; set; }
+        public DataTemplate SymptomTemplate { get; set; }
+
+        public override DataTemplate
+              SelectTemplate(object item, DependencyObject container)
+        {
+            if (item != null)
+            {
+                if (item is DiagnosisViewModel)
+                {
+                    return DiagnosisTemplate;
+                }
+                if (item is SymptomViewModel)
+                {
+                    return SymptomTemplate;
+                }
+            }
+
+            return null;
+        }
+    }
+}
