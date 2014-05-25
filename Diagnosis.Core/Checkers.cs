16afd992294a65f7464ed314222838d326c5f1db
smg <semagerasimov@gmail.com>
2014-05-25 16:08:52 +0700

    move models to Core project

--- Diagnosis.Core/Checkers.cs
+++ Diagnosis.Core/Checkers.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Diagnosis.Models
+namespace Diagnosis.Core
 {
     static class Checkers
     {
--- Diagnosis.Core/Models/Patient.cs
+++ Diagnosis.Core/Models/Patient.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics.Contracts;
 using System.Linq;
+using Diagnosis.Core;
 
 namespace Diagnosis.Models
 {
