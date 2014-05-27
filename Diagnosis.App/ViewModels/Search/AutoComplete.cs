6b4c6c536513bc26bf140a799fd0d9b83e56130e
smg <semagerasimov@gmail.com>
2014-05-27 10:22:21 +0700

    AutoComplete with QuerySeparator

--- Diagnosis.App/ViewModels/Search/AutoComplete.cs
+++ Diagnosis.App/ViewModels/Search/AutoComplete.cs
@@ -3,17 +3,19 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Windows.Controls;
+using System.Diagnostics.Contracts;
 using System.Windows.Input;
 
 namespace Diagnosis.App.ViewModels
 {
     public class AutoComplete : ViewModelBase
     {
+        readonly QuerySeparator separator;
+
         private SearchWrap _selected;
         private RelayCommand _selectCommand;
-
-
         private string _query;
+
         public string Query
         {
             get
@@ -23,41 +25,22 @@ namespace Diagnosis.App.ViewModels
             set
             {
                 Console.WriteLine("Query: {0}", value);
-                var trimed = value.Trim();
+
+                var trimed = separator.TrimExcessDelimiters(value);
                 if (_query != trimed)
                 {
+                    MakeSuggestions(separator.RemoveSpacers(trimed));
+
                     _query = trimed;
 
-                    EntityManagers.WordsManager.WipeUnsaved();
-                    EntityManagers.SymptomsManager.WipeUnsaved();
-
-                    var symptoms = new List<SymptomViewModel>();
-                    foreach (var line in StringSequencePartition.Part(_query))
-                    {
-                        Console.Write("line of partition: ");
-                        foreach (var word in line)
-                        {
-                            Console.Write("{0}, ", word);
-                        }
-                        Console.WriteLine();
-
-                        // группы из слов, начинающихся на слово из разбиения
-                        var wordGroups = line.Select(word => EntityManagers.WordsManager.Searcher.Search(word).ToList()).ToList();
-
-                        // все комбинации слов для создания симптомов
-                        var wordsForSymptoms = Combinator<WordViewModel>.Combine(wordGroups);
-
-                        foreach (var words in wordsForSymptoms)
-                        {
-                            symptoms.Add(EntityManagers.SymptomsManager.Create(words));
-                        }
-                    }
-                    Results = new ObservableCollection<SearchWrap>(symptoms.Select(s => new SearchWrap(s)));
-                    OnPropertyChanged(() => Results);
+                    OnPropertyChanged(() => Query);
+                }
+                else
+                {
+                    Console.WriteLine("same query");
                 }
             }
         }
-
         public ObservableCollection<SearchWrap> Results { get; private set; }
 
         public SearchWrap Selected
@@ -81,81 +64,50 @@ namespace Diagnosis.App.ViewModels
             {
                 return _selectCommand ?? (_selectCommand = new RelayCommand(() =>
                                           {
-                                              var wvm = Selected.entity as WordViewModel;
-                                              if (wvm != null)
+                                              var svm = Selected.entity as SymptomViewModel;
+                                              if (svm != null)
                                               {
-                                                  wvm.checkable.IsChecked = true;
-                                              }
-                                              else
-                                              {
-                                                  var svm = Selected.entity as SymptomViewModel;
-                                                  if (svm != null)
-                                                  {
-                                                      svm.Words.ForAll(w => w.checkable.IsChecked = true);
-                                                  }
+                                                  svm.Words.ForAll(w => w.checkable.IsChecked = true);
                                               }
                                           }));
             }
         }
 
-        public AutoCompleteFilterPredicate<object> ItemFilter
+        private void MakeSuggestions(string query)
         {
-            get
-            {
-                return new AutoCompleteFilterPredicate<object>((query, obj) => FilterItem(query, obj as SearchWrap));
-            }
-        }
+            EntityManagers.WordsManager.WipeUnsaved();
+            EntityManagers.SymptomsManager.WipeUnsaved();
 
-        public static bool FilterItem(string query, SearchWrap wrap)
-        {
-            var vm = wrap.entity;
-            var wvm = vm as WordViewModel;
-            if (wvm != null)
+            var symptoms = new List<SymptomViewModel>();
+            foreach (var line in StringSequencePartition.Part(query, separator.Delimiter))
             {
-                return wvm.Name.StartsWith(query, StringComparison.InvariantCultureIgnoreCase);
-            }
-            else
-            {
-                var svm = vm as SymptomViewModel;
-                if (svm != null)
+                Console.Write("line of partition: ");
+                foreach (var word in line)
                 {
-                    foreach (var line in StringSequencePartition.Part(query))
-                    {
-                        bool result = false;
+                    Console.Write("{0}, ", word);
+                }
+                Console.WriteLine();
 
-                        // VM слов из разбиения
-                        var words = line.Select(word => EntityManagers.WordsManager.Find(word)).ToList();
+                // группы из слов, начинающихся на слово из разбиения
+                var wordGroups = line.Select(word => EntityManagers.WordsManager.RootSearcher.Search(word).ToList()).ToList();
 
-                        // в запросе может быть часть слова из базы или слово, которого нет в базе
-                        // ищем слова или части слов, которых нет в симптоме
+                // все комбинации слов для создания симптомов
+                var wordsForSymptoms = Combinator<WordViewModel>.Combine(wordGroups);
 
-                        for (int i = 0; i < line.Count; i++)
-                        {
-                            if (words[i] == null)
-                            {
-                                // ложь, если в симптоме нет ни одного слова, начинающегося на часть запроса (группу в линии)
-                                result = svm.Words.Any(w => w.Name.StartsWith(line[i], StringComparison.InvariantCultureIgnoreCase));
-                            }
-                            else
-                            {
-                                // ложь, если в симптоме нет некоторого слова из группы
-                                result = svm.Words.Contains(words[i]);
-                            }
-                            if (!result)
-                                break;
-                        }
-
-                        if (result)
-                            return true;
-                    }
+                foreach (var words in wordsForSymptoms)
+                {
+                    symptoms.Add(EntityManagers.SymptomsManager.Create(words));
                 }
             }
-
-            return false;
+            Results = new ObservableCollection<SearchWrap>(symptoms.Select(s => new SearchWrap(s)));
+            OnPropertyChanged(() => Results);
         }
 
-        public AutoComplete()
+        public AutoComplete(QuerySeparator separator)
         {
+            Contract.Requires(separator != null);
+
+            this.separator = separator;
             Results = new ObservableCollection<SearchWrap>();
         }
     }
