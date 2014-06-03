fa3d6e44dae9275d7513fe0cc0cab6f01c1fca91
smg <semagerasimov@gmail.com>
2014-06-03 14:12:27 +0700

    add IEditableNesting

---
+++ Diagnosis.App/ViewModels/Extensions/IEditableNestingExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Diagnostics.Contracts;
+
+namespace Diagnosis.App.ViewModels
+{
+    static class IEditableNestingExtensions
+    {
+        public static void DeleteEmpty<T>(this IEditableNesting outer, IList<T> inners) where T : IEditableNesting
+        {
+            var i = 0;
+            while (i < inners.Count)
+            {
+                if (inners[i].IsEmpty)
+                {
+                    inners[i].Editable.DeleteCommand.Execute(null);
+                }
+                else
+                {
+                    i++;
+                }
+            }
+        }
+    }
+}
---
+++ Diagnosis.App/ViewModels/Interfaces/IEditableNesting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diagnosis.App.ViewModels
+{
+    interface IEditableNesting
+    {
+        Editable Editable { get; }
+        bool IsEmpty { get; }
+    }
+}
