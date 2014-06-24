f5325aab0dd1b452d8ea497d708b2795d3449398
smg <semagerasimov@gmail.com>
2014-06-24 22:50:51 +0700

    VM base is IDisposable

--- Diagnosis.App/ViewModels/ViewModelBase.cs
+++ Diagnosis.App/ViewModels/ViewModelBase.cs
@@ -1,8 +1,30 @@
-﻿
+﻿using System;
+
 namespace Diagnosis.App.ViewModels
 {
-    public abstract class ViewModelBase : Diagnosis.Core.NotifyPropertyChangedBase
+    public abstract class ViewModelBase : Diagnosis.Core.NotifyPropertyChangedBase, IDisposable
     {
+        #region IDisposable Members
 
+        public void Dispose()
+        {
+            this.OnDispose();
+        }
+
+        protected virtual void OnDispose()
+        {
+        }
+
+#if DEBUG
+
+        ~ViewModelBase()
+        {
+            string msg = string.Format("{0} ({1}) ({2}) Finalized", this.GetType().Name, this.ToString(), this.GetHashCode());
+            System.Diagnostics.Debug.WriteLine(msg);
+        }
+
+#endif
+
+        #endregion IDisposable Members
     }
 }
