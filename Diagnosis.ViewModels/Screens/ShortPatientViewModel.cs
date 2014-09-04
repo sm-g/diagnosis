7b5fad281e6a34ed3a7785016c161e32cbf05c91
smg <semagerasimov@gmail.com>
2014-09-04 13:13:10 +0700

    add ShortPatientVM

---
+++ Diagnosis.ViewModels/Screens/ShortPatientViewModel.cs
@@ -0,0 +1,203 @@
+﻿using Diagnosis.Core;
+using Diagnosis.Models;
+using EventAggregator;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Windows.Input;
+
+namespace Diagnosis.ViewModels
+{
+    public class ShortPatientViewModel : CheckableBase
+    {
+        internal readonly Patient patient;
+
+        #region Model related
+
+        public string Label
+        {
+            get
+            {
+                return patient.Label;
+            }
+            set
+            {
+                if (patient.Label != value)
+                {
+                    patient.Label = value;
+                    OnPropertyChanged("Label");
+                }
+            }
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return patient.FirstName ?? "";
+            }
+            set
+            {
+                if (patient.FirstName != value)
+                {
+                    patient.FirstName = value;
+
+                    OnPropertyChanged("FirstName");
+                    OnPropertyChanged("NoName");
+                }
+            }
+        }
+
+        public string MiddleName
+        {
+            get
+            {
+                return patient.MiddleName ?? "";
+            }
+            set
+            {
+                if (patient.MiddleName != value)
+                {
+                    patient.MiddleName = value;
+
+                    OnPropertyChanged("MiddleName");
+                    OnPropertyChanged("SearchText");
+                    OnPropertyChanged("NoName");
+                    OnPropertyChanged("Self");
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return patient.LastName ?? "";
+            }
+            set
+            {
+                if (patient.LastName != value)
+                {
+                    patient.LastName = value;
+
+                    OnPropertyChanged("LastName");
+                    OnPropertyChanged("SearchText");
+                    OnPropertyChanged("NoName");
+                    OnPropertyChanged("Self");
+                }
+            }
+        }
+
+        public int? Age
+        {
+            get
+            {
+                return patient.Age;
+            }
+            set
+            {
+                if (patient.Age != value)
+                {
+                    patient.Age = value;
+                    OnPropertyChanged("Age");
+                    OnPropertyChanged("BirthYear");
+                }
+            }
+        }
+
+        public int? BirthYear
+        {
+            get
+            {
+                return patient.BirthYear;
+            }
+            set
+            {
+                if (patient.BirthYear != value)
+                {
+                    patient.BirthYear = value;
+                    OnPropertyChanged("Age");
+                    OnPropertyChanged("BirthYear");
+                }
+            }
+        }
+
+        public byte? BirthMonth
+        {
+            get
+            {
+                return patient.BirthMonth;
+            }
+            set
+            {
+                if (patient.BirthMonth != value)
+                {
+                    patient.BirthMonth = value;
+                    OnPropertyChanged("Age");
+                    OnPropertyChanged("BirthMonth");
+                }
+            }
+        }
+
+        public byte? BirthDay
+        {
+            get
+            {
+                return patient.BirthDay;
+            }
+            set
+            {
+                if (patient.BirthDay != value)
+                {
+                    patient.BirthDay = value;
+                    OnPropertyChanged("Age");
+                    OnPropertyChanged("BirthDay");
+                }
+            }
+        }
+
+        public bool IsMale
+        {
+            get
+            {
+                return patient.IsMale;
+            }
+            set
+            {
+                if (patient.IsMale != value)
+                {
+                    patient.IsMale = value;
+                    OnPropertyChanged("IsMale");
+                }
+            }
+        }
+
+        public bool NoName
+        {
+            get
+            {
+                return patient.LastName == null && patient.MiddleName == null && patient.FirstName == null;
+            }
+        }
+
+        #endregion Model related
+
+
+        public ShortPatientViewModel(Patient p)
+        {
+            Contract.Requires(p != null);
+            this.patient = p;
+
+
+        }
+
+
+        public override string ToString()
+        {
+            return patient.ToString();
+        }
+    }
+}
