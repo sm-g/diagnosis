3ecf05491549fcb6bbfa6b2cfe26a0816164788e
smg <semagerasimov@gmail.com>
2014-09-10 23:29:13 +0700

    override Equals and GetHashCode for EntityBase

--- Diagnosis.Models/EntityBase.cs
+++ Diagnosis.Models/EntityBase.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using Diagnosis.Core;
+﻿using Diagnosis.Core;
+using System;
 
 namespace Diagnosis.Models
 {
@@ -12,6 +9,8 @@ namespace Diagnosis.Models
 
     public class EntityBase : NotifyPropertyChangedBase, IEntity
     {
+        private int? cachedHashCode;
+
         public virtual int Id { get; protected set; }
 
         /// <summary>
@@ -35,6 +34,52 @@ namespace Diagnosis.Models
         public virtual bool IsTransient
         {
             get { return Id == 0; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return EntityEquals(obj as EntityBase);
+        }
+
+        public override int GetHashCode()
+        {
+            if (cachedHashCode.HasValue) return cachedHashCode.Value;
+
+            cachedHashCode = IsTransient ? base.GetHashCode() : Id.GetHashCode();
+            return cachedHashCode.Value;
+        }
+
+        protected bool EntityEquals(EntityBase other)
+        {
+            if (other == null)
+            {
+                return false;
+            }
+            else if (IsTransient && other.IsTransient)
+            {
+                // both entities are not saved
+                return ReferenceEquals(this, other);
+            }
+            else
+            {
+                // one of entities saved.
+                return Id == other.Id;
+            }
+        }
+        // Maintain equality operator semantics for entities.
+        public static bool operator ==(EntityBase x, EntityBase y)
+        {
+            // By default, == and Equals compares references. In order to 
+            // maintain these semantics with entities, we need to compare by 
+            // identity value. The Equals(x, y) override is used to guard 
+            // against null values; it then calls EntityEquals().
+            return Object.Equals(x, y);
+        }
+
+        // Maintain inequality operator semantics for entities. 
+        public static bool operator !=(EntityBase x, EntityBase y)
+        {
+            return !(x == y);
         }
     }
 }
