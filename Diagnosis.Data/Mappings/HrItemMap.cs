011b0c472047a00a27fc9a0256cd9475988719d3
smg <semagerasimov@gmail.com>
2014-10-02 10:02:46 +0700

    add HrItem
    --HG--
    branch : data

---
+++ Diagnosis.Data/Mappings/HrItemMap.cs
@@ -0,0 +1,34 @@
+﻿using Diagnosis.Models;
+using NHibernate.Mapping.ByCode;
+using NHibernate.Mapping.ByCode.Conformist;
+
+namespace Diagnosis.Data.Mappings
+{
+    public class HrItemMap : ClassMapping<HrItem>
+    {
+        public HrItemMap()
+        {
+            Id(x => x.Id, m =>
+            {
+                m.Generator(Generators.Native);
+            });
+
+            Property(x => x.Order, m => m.Column("Ord"));
+
+            ManyToOne(x => x.Word, m =>
+            {
+                m.Column("WordID");
+            });
+            // Property(x => x.Measure);
+            ManyToOne(x => x.Disease, m =>
+            {
+                m.Column("DiseaseID");
+            });
+            ManyToOne(x => x.HealthRecord, m =>
+            {
+                m.Column("HealthRecordID");
+                m.NotNullable(true);
+            });
+        }
+    }
+}
---
+++ Diagnosis.Models/Entities/HrItem.cs
@@ -0,0 +1,100 @@
+﻿using Diagnosis.Core;
+using Iesi.Collections.Generic;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Diagnostics.Contracts;
+
+namespace Diagnosis.Models
+{
+    public class HrItem : EntityBase, IDomainEntity
+    {
+
+        private IcdDisease _disease;
+        private Word _word;
+        private Measure _measure;
+
+        public HrItem(HealthRecord hr, Word word)
+        {
+            Contract.Requires(hr != null);
+
+            HealthRecord = hr;
+            Word = word;
+        }
+
+        public HrItem(HealthRecord hr, Measure measure)
+        {
+            Contract.Requires(hr != null);
+
+            HealthRecord = hr;
+            Measure = measure;
+        }
+
+        protected HrItem()
+        {
+        }
+
+        public virtual HealthRecord HealthRecord { get; protected set; }
+        public virtual IcdDisease Disease
+        {
+            get { return _disease; }
+            protected set
+            {
+                if (_disease == value)
+                    return;
+                EditHelper.Edit(() => Disease);
+                _disease = value;
+                OnPropertyChanged("Disease");
+            }
+        }
+        public virtual Word Word
+        {
+            get
+            {
+                return _word;
+            }
+            protected set
+            {
+                if (_word != value)
+                {
+                    _word = value;
+                    OnPropertyChanged(() => Word);
+                }
+            }
+        }
+        public virtual Measure Measure
+        {
+            get
+            {
+                return _measure;
+            }
+            protected set
+            {
+                if (_measure != value)
+                {
+                    _measure = value;
+                    OnPropertyChanged(() => Measure);
+                }
+            }
+        }
+
+        public virtual IDomainEntity Entity
+        {
+            get
+            {
+                if (Word != null) return Word;
+                if (Measure != null) return Measure;
+                if (Disease != null) return Disease;
+
+                return null;
+            }
+        }
+
+        public virtual int Order { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ", Id);
+        }
+
+    }
+}
