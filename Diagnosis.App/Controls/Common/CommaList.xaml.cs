9bd9e085fada344cfdcd4b692d88e4a535624416
smg <semagerasimov@gmail.com>
2014-10-08 21:22:58 +0700

    controls. CommaList with Separator dep prop
    still need to fix Visibility of separator

--- Diagnosis.App/Controls/Common/CommaList.xaml.cs
+++ Diagnosis.App/Controls/Common/CommaList.xaml.cs
@@ -11,17 +11,51 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Collections;
 
 namespace Diagnosis.App.Controls
 {
     /// <summary>
     /// Interaction logic for CommaList.xaml
     /// </summary>
-    public partial class CommaList : ItemsControl
+    public partial class CommaList : UserControl
     {
         public CommaList()
         {
             InitializeComponent();
+            if (Separator == null)
+                Separator = new TextBlock() { Text = ", " };
         }
+
+        public FrameworkElement Separator
+        {
+            get { return (FrameworkElement)GetValue(SeparatorProperty); }
+            set { SetValue(SeparatorProperty, value); }
+        }
+
+        public static readonly DependencyProperty SeparatorProperty =
+            DependencyProperty.Register("Separator", typeof(FrameworkElement), typeof(CommaList));
+
+        public IEnumerable ItemsSource
+        {
+            get { return (IEnumerable)GetValue(ItemsSourseProperty); }
+            set { SetValue(ItemsSourseProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemsSourseProperty =
+            DependencyProperty.Register("ItemsSource", typeof(IEnumerable), typeof(CommaList));
+
+
+
+        public DataTemplate ItemTemplate
+        {
+            get { return (DataTemplate)GetValue(ItemTemplateProperty); }
+            set { SetValue(ItemTemplateProperty, value); }
+        }
+
+        public static readonly DependencyProperty ItemTemplateProperty =
+            DependencyProperty.Register("ItemTemplate", typeof(DataTemplate), typeof(CommaList));
+
+        
     }
 }
