bbf496f2df5161de41b321965c57838c0ab092ae
smg <semagerasimov@gmail.com>
2014-10-08 21:24:36 +0700

    controls. Add InList converter

---
+++ Diagnosis.App/Converters/InList.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace Diagnosis.App.Converters
+{
+    public class InListConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Length < 2)
+                return false;
+            var col = values[1] as IList;
+            if (col == null)
+                return false;
+
+            return col.Contains(values[0]);
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
