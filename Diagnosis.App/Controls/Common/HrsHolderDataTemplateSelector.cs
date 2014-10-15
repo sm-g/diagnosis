d87a43929d17ba0fbff09e9b999a65808edfd993
smg <semagerasimov@gmail.com>
2014-10-15 10:08:29 +0700

    xaml. Replace two HrsHolderDataTemplateSelectors with only one
    for IHrsHolder and VMs

--- Diagnosis.App/Controls/Common/HrsHolderDataTemplateSelector.cs
+++ Diagnosis.App/Controls/Common/HrsHolderDataTemplateSelector.cs
@@ -6,8 +6,9 @@ using System.Windows;
 using System.Windows.Controls;
 using Diagnosis.ViewModels;
 using Diagnosis.Models;
+using System.Diagnostics;
 
-namespace Diagnosis.App.Controls.Search
+namespace Diagnosis.App.Controls
 {
     public class HrsHolderDataTemplateSelector : DataTemplateSelector
     {
@@ -17,20 +18,36 @@ namespace Diagnosis.App.Controls.Search
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            if (item is Patient)
+            if (item == null)
+                return null;
+
+            var @switch = new Dictionary<Type, Func<DataTemplate>> {
+                { typeof(Patient), () => PatientTemplate },
+                { typeof(Course), () => CourseTemplate },
+                { typeof(Appointment), () => AppointmentTemplate }
+           };
+
+            if (item is IHrsHolder)
+            {
+                return @switch[item.GetType()]();
+            }
+
+            if (item is PatientViewModel)
             {
                 return PatientTemplate;
             }
-            if (item is Course)
+            if (item is CourseViewModel)
             {
                 return CourseTemplate;
             }
-            if (item is Appointment)
+            if (item is AppointmentViewModel)
             {
                 return AppointmentTemplate;
             }
 
-            return null;
+            dynamic vm = item; // vm with Holder prop
+
+            return @switch[vm.Holder.GetType()]();
         }
     }
 
