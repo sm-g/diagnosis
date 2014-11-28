0d0ec421cf38b2e429ee088a6bc958c76fb734c8
smg <semagerasimov@gmail.com>
2014-11-28 20:37:38 +0700

    add LessThanToVisibility converter

--- Diagnosis.App/Converters/CountToVisibility.cs
+++ Diagnosis.App/Converters/CountToVisibility.cs
@@ -4,7 +4,7 @@ using System.Windows.Data;
 
 namespace Diagnosis.App.Converters
 {
-    public class CountToVisibility : IValueConverter
+    public class MoreThanToVisibility : IValueConverter
     {
         public object Convert(object value, Type targetType,
             object parameter, CultureInfo culture)
@@ -16,6 +16,31 @@ namespace Diagnosis.App.Converters
                 int.TryParse((string)parameter, out limit);
 
             if (count > limit)
+            {
+                return System.Windows.Visibility.Visible;
+            }
+            return System.Windows.Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+
+    public class LessThanToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType,
+            object parameter, CultureInfo culture)
+        {
+            int count = (int)value;
+            int limit = 1;
+
+            if (parameter != null)
+                int.TryParse((string)parameter, out limit);
+
+            if (count < limit)
             {
                 return System.Windows.Visibility.Visible;
             }
