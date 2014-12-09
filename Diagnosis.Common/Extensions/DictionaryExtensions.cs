28d848e3bded53190db743e8eea28caf04f11b24
smg <semagerasimov@gmail.com>
2014-12-09 14:20:28 +0700

    data. Fix db backup

---
+++ Diagnosis.Common/Extensions/DictionaryExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Diagnosis.Common
+{
+    public static class DictionaryExtensions
+    {
+        /// <summary>
+        /// Return dict value or default value of TVal or empty collection.
+        /// </summary>
+        public static TVal GetValueOrDefault<TKey, TVal>(this IDictionary<TKey, TVal> dict, TKey key) where TVal : new()
+        {
+
+            TVal result;
+            if (dict.TryGetValue(key, out result))
+                return result;
+            else
+            {
+                if (typeof(IEnumerable).IsAssignableFrom(typeof(TVal)))
+                {
+                    return new TVal();
+                }
+                return default(TVal);
+            }
+        }
+    }
+}
--- Diagnosis.Common/Util/FileHelper.cs
+++ Diagnosis.Common/Util/FileHelper.cs
@@ -1,23 +1,27 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace Diagnosis.Common
 {
     public static class FileHelper
     {
-        // from http://www.rajapet.com/2014/03/a-file-versioning-helper-class-in-c-to-make-a-backup-copy-of-a-file-and-keep-the-last-n-copies-of-that-file.html
         /// <summary>
-        /// Make a numbered backup copy of the specified file.  Backup files have the name filename.exe.yymmdd##, where yymmdd is the date and ## is a zero justified sequence number starting at 1
+        /// Make a numbered backup copy of the specified file. 
+        /// Backup files have the name yymmdd-##-filename, where yymmdd is the date
+        /// and ## is a zero justified sequence number starting at 0
         /// </summary>
         /// <param name="fileName">Name of the file to backup.</param>
         /// <param name="backupDir">Backup folder.</param>
-        /// <param name="maxBackupsPerDay">The maximum backups to keep, up to 99.</param>
-        public static void Backup(string fileName, string backupDir, int maxBackupsPerDay)
+        /// <param name="maxBackupsPerDay">The maximum backups to keep per day, up to 99.</param>
+        /// <param name="maxDaysToKeep">The maximum distinct days to keep.</param>
+        /// <remarks>based on http://www.rajapet.com/2014/03/a-file-versioning-helper-class-in-c-to-make-a-backup-copy-of-a-file-and-keep-the-last-n-copies-of-that-file.html </remarks>
+        public static void Backup(string fileName, string backupDir, int maxBackupsPerDay = 5, int maxDaysToKeep = 7)
         {
-            maxBackupsPerDay = Math.Min(maxBackupsPerDay, 99);
+            var maxNumber = 99;
+            maxBackupsPerDay = Math.Min(Math.Max(maxBackupsPerDay, 1), maxNumber); // from 1 to 99
+            maxDaysToKeep = Math.Max(maxDaysToKeep, 1); // from 1
+
             var file = new FileInfo(fileName);
             if (file.Exists)
             {
@@ -26,46 +30,78 @@ namespace Diagnosis.Common
                 if (!dir.Exists)
                     dir.Create();
 
-                int newSequence = 0;
-
                 // Get the list of previous backups of the file, skipping the current file
+                // do not touch non-backup files
                 var backupFiles = Directory.GetFiles(backupDir, "*" + file.Name)
                     .ToList()
-                    .Where(d => !d.Equals(file.FullName) && Path.GetFileName(d).Length == 10 + fileName.Length) // 10 - date and number
+                    .Where(d => !d.Equals(file.FullName) && Path.GetFileName(d).Length == 10 + fileName.Length) // 10 - date and number length
                     .OrderBy(d => d);
 
-                // Get the name of the last backup performed
-                var lastBackupFilename = backupFiles.LastOrDefault();
+                var byDays = (from f in backupFiles
+                              group f by f.Substring(backupDir.Length, 6) into gr
+                              select new
+                              {
+                                  Date = gr.Key,
+                                  Files = gr.ToList()
+                              }).ToDictionary(x => x.Date, x => x.Files);
 
-                // If we have at least one previous backup copy
-                if (lastBackupFilename != null)
+                // delete out of date files
+                var expiredDates = byDays.Keys
+                    .Take(byDays.Keys.Count - maxDaysToKeep);
+                foreach (var date in expiredDates)
                 {
-                    // Get the last sequence number back taking the 2 middle characters and convert them to an int. And add 1 to that number
-                    string number = "";
-                    try
-                    {
-                        number = Path.GetFileName(lastBackupFilename).Substring(7, 2);
-                        if (Int32.TryParse(number, out newSequence))
-                            newSequence++;
-                    }
-                    catch // not this backup, will always rewrite 00
-                    {
-                    }
+                    byDays[date].ForEach(f => File.Delete(f));
+                }
 
-                    if (backupFiles.Count() >= maxBackupsPerDay)
-                    {
-                        // Get a list of the oldest files to delele
-                        var expiredFiles = backupFiles.Take(backupFiles.Count() - maxBackupsPerDay + 1);
+                // note - we leave all backups between (today - maxDaysToKeep) and today
 
+                int newSequence = 0;
+                var today = String.Format("{0:yyMMdd}", DateTime.Now);
+
+                Action<string, int> rename = (oldName, seqNum) =>
+                {
+                    var newName = new FileInfo(Path.Combine(dir.Name,
+                                    String.Format("{0}-{1:00}-{2}", today, seqNum, file.Name)));
+                    File.Move(oldName, newName.FullName);
+                };
+
+                // If we have at least one today backup copy
+                var lastTodayBackup = byDays.GetValueOrDefault(today).LastOrDefault();
+                if (lastTodayBackup != null)
+                {
+                    // delete oldest todays backups
+                    var todayFiles = byDays[today];
+                    var todayCopiesCount = todayFiles.Count;
+                    if (todayCopiesCount >= maxBackupsPerDay)
+                    {
+                        var expiredFiles = backupFiles.Take(todayCopiesCount - maxBackupsPerDay + 1);
                         foreach (var expiredFile in expiredFiles)
                         {
                             File.Delete(expiredFile);
+                            todayFiles.Remove(expiredFile);
                         }
                     }
-                }
-                var backupFile = new FileInfo(Path.Combine(dir.Name,
-                    String.Format("{0:yyMMdd}-{1:00}-{2}", DateTime.Now, newSequence, file.Name)));
 
+                    // Get the last sequence number back taking the 2 middle characters and convert them to an int. And add 1 to that number
+                    string number = Path.GetFileName(lastTodayBackup).Substring(7, 2);
+                    if (Int32.TryParse(number, out newSequence))
+                        newSequence++;
+
+                    if (newSequence > maxNumber)
+                    {
+                        // rename all files starting from zero
+                        newSequence = 0;
+                        todayFiles.ForEach(f =>
+                        {
+                            rename(f, newSequence);
+                            newSequence++;
+                        });
+                    }
+                }
+
+                // new backup
+                var backupFile = new FileInfo(Path.Combine(dir.Name,
+                                    String.Format("{0}-{1:00}-{2}", today, newSequence, file.Name)));
                 File.Copy(file.FullName, backupFile.FullName, true);
             }
         }
