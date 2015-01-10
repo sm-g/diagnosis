ed91d4825580d1e1c7907fd9f4d8b97a5d00a7ed
smg <semagerasimov@gmail.com>
2015-01-10 12:33:45 +0700

    ui. Hide Title on screen pane

--- Diagnosis.App/Windows/Shell/AvalonTemplateSelector.cs
+++ Diagnosis.App/Windows/Shell/AvalonTemplateSelector.cs
@@ -6,25 +6,33 @@ using System.Windows;
 using System.Windows.Controls;
 using Diagnosis.Models;
 using System.Diagnostics;
+using Xceed.Wpf.AvalonDock.Layout;
 using Diagnosis.ViewModels.Screens;
 
 namespace Diagnosis.App.Windows.Shell
 {
     public class AvalonTemplateSelector : DataTemplateSelector
     {
+        Dictionary<Type, DataTemplate> @sw;
+
+        public AvalonTemplateSelector()
+        {
+            @sw = new Dictionary<Type, DataTemplate> { 
+                { typeof(ToolViewModel), PanelTemplate},
+                { typeof(ScreenBase), ScreenTemplate}                                                
+            };
+        }
+
         public DataTemplate PanelTemplate { get; set; }
         public DataTemplate ScreenTemplate { get; set; }
 
         public override DataTemplate SelectTemplate(object item, DependencyObject container)
         {
-            if (item is ToolViewModel)
+            var lc = item as LayoutContent;
+            if (lc != null)
             {
-                return PanelTemplate;
-            }
-
-            if (item is ScreenBase)
-            {
-                return ScreenTemplate;
+                if (lc.Content is ToolViewModel) return PanelTemplate;
+                if (lc.Content is ScreenBase) return ScreenTemplate;
             }
 
             return base.SelectTemplate(item, container);
--- Diagnosis.ViewModels/Screens/Shell/AvalonDock/PaneViewModel.cs
+++ Diagnosis.ViewModels/Screens/Shell/AvalonDock/PaneViewModel.cs
@@ -114,7 +114,7 @@ namespace Diagnosis.ViewModels.Screens
         //    }
         //}
 
-        public RelayCommand NoAutoHideCommand
+        public RelayCommand NothingCommand
         {
             get
             {
