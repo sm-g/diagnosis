b731361466a6a1a6b940686ee7f7ce0c1734af1b
smg <semagerasimov@gmail.com>
2015-02-03 15:07:54 +0700

    model. Validatable Uom

--- Diagnosis.Models/Entities/Uom.cs
+++ Diagnosis.Models/Entities/Uom.cs
@@ -7,23 +7,51 @@ using System.Linq;
 using System.Text;
 using System.Collections.Specialized;
 using Iesi.Collections.Generic;
+using FluentValidation.Results;
+using Diagnosis.Models.Validators;
 
 namespace Diagnosis.Models
 {
     [Serializable]
 
-    public class Uom : EntityBase<int>, IDomainObject
+    public class Uom : ValidatableEntity<int>, IDomainObject
     {
         public static Uom Null = new Uom("—", 1, -1);  // для измерения без единицы
+        private string _description;
+        private string _abbr;
+        private double _factor;
 
-        public virtual string Abbr { get; protected set; }
-        public virtual string Description { get; set; }
+        public virtual string Abbr
+        {
+            get { return _abbr; }
+            set
+            {
+                var filtered = value.Trim();
+                SetProperty(ref _abbr, filtered, () => Abbr);
+            }
+        }
+        public virtual string Description
+        {
+            get { return _description; }
+            set
+            {
+                var filtered = value.Trim();
+                SetProperty(ref _description, filtered, () => Description);
+            }
+        }
         /// <summary>
         /// Показатель степени множителя по основанию 10 относительно
         /// базовой единицы этого типа (единицы объема: -3 для мкл, 0 для мл).
         /// При сохранении в БД Measure.Value = Value * 10^Factor.
         /// </summary>
-        public virtual double Factor { get; set; }
+        public virtual double Factor
+        {
+            get { return _factor; }
+            set
+            {
+                SetProperty(ref _factor, value, () => Factor);
+            }
+        }
         public virtual int Type { get; set; }
 
         public Uom(string abbr, double factor, int type)
@@ -42,5 +70,10 @@ namespace Diagnosis.Models
             return string.Format("{0}", Abbr);
         }
 
+
+        public override ValidationResult SelfValidate()
+        {
+            return new UomValidator().Validate(this);
+        }
     }
 }
---
+++ Diagnosis.Models/Validators/UomValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using FluentValidation.Results;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Diagnosis.Common;
+
+namespace Diagnosis.Models.Validators
+{
+    public class UomValidator : AbstractValidator<Uom>
+    {
+        public UomValidator()
+        {
+            RuleFor(w => w.Description).Length(1, 100);
+            RuleFor(w => w.Abbr).NotNull().Length(1, 10);
+        }
+    }
+}
