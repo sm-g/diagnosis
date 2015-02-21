ef1eeac0432e82baa60ba2c2d4c3f1764a54e566
smg <semagerasimov@gmail.com>
2015-02-21 10:52:13 +0700

    Add version 1.5.1 (without sync framework)

--- VersionInfo.cs
+++ VersionInfo.cs
@@ -8,8 +8,8 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Reflection.AssemblyVersion("1.5.0")]
-[assembly: System.Reflection.AssemblyFileVersion("1.5.0")]
-[assembly: System.Reflection.AssemblyInformationalVersion("1.5.0")]
+[assembly: System.Reflection.AssemblyVersion("1.5.1")]
+[assembly: System.Reflection.AssemblyFileVersion("1.5.1")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.5.1")]
 
 
