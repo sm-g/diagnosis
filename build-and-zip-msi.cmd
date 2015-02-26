77394d963e5803aeede857dd86cfbe54b4ade60f
smg <semagerasimov@gmail.com>
2015-02-26 21:53:33 +0700

    build. Fixes for setup

---
+++ build-and-zip-msi.cmd
@@ -0,0 +1,4 @@
+@echo off
+rem app, build, zip, CopyWorking
+call build.cmd client 0 1 0
+call build.cmd server 0 1 0
--- build-release-and-zip-msi.cmd
+++ build-release-and-zip-msi.cmd
@@ -1,4 +1,4 @@
 @echo off
 rem app, build, zip, CopyWorking
-build.cmd server 1 1 0
-build.cmd client 1 1 0
+call build.cmd server 1 1 0
+call build.cmd client 1 1 0
