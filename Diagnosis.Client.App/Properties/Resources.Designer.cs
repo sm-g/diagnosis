3b20ae3cc58ba8bccc0a4708c6261ae30ba88ebc
smg <semagerasimov@gmail.com>
2015-02-26 20:28:35 +0700

    code. Rename App and Setup projects

--- Diagnosis.Client.App/Properties/Resources.Designer.cs
+++ Diagnosis.Client.App/Properties/Resources.Designer.cs
@@ -8,10 +8,11 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Diagnosis.App.Properties {
+namespace Diagnosis.App.Properties
+{
     using System;
-    
-    
+
+
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,74 +23,88 @@ namespace Diagnosis.App.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
-        
+    public class Resources
+    {
+
         private static global::System.Resources.ResourceManager resourceMan;
-        
+
         private static global::System.Globalization.CultureInfo resourceCulture;
-        
+
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources()
+        {
         }
-        
+
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Resources.ResourceManager ResourceManager {
-            get {
-                if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Diagnosis.App.Properties.Resources", typeof(Resources).Assembly);
+        public static global::System.Resources.ResourceManager ResourceManager
+        {
+            get
+            {
+                if (object.ReferenceEquals(resourceMan, null))
+                {
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Diagnosis.Client.App.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-        
+
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        public static global::System.Globalization.CultureInfo Culture {
-            get {
+        public static global::System.Globalization.CultureInfo Culture
+        {
+            get
+            {
                 return resourceCulture;
             }
-            set {
+            set
+            {
                 resourceCulture = value;
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap _126_Edit_24x24_72 {
-            get {
+        public static System.Drawing.Bitmap _126_Edit_24x24_72
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("_126_Edit_24x24_72", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap AddControl_371 {
-            get {
+        public static System.Drawing.Bitmap AddControl_371
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("AddControl_371", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap Arrow_UndoRevertRestore_16xLG {
-            get {
+        public static System.Drawing.Bitmap Arrow_UndoRevertRestore_16xLG
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("Arrow_UndoRevertRestore_16xLG", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to &lt;?xml version=&quot;1.0&quot; encoding=&quot;utf-16&quot;?&gt;
         ///&lt;LayoutRoot xmlns:xsi=&quot;http://www.w3.org/2001/XMLSchema-instance&quot; xmlns:xsd=&quot;http://www.w3.org/2001/XMLSchema&quot;&gt;
@@ -97,149 +112,181 @@ namespace Diagnosis.App.Properties {
         ///    &lt;LayoutAnchorablePane Id=&quot;7896874a-62cd-4d5b-b5f5-01f3f3e0ad4e&quot; Name=&quot;Screen&quot; DocMinWidth=&quot;320&quot;&gt;
         ///      &lt;LayoutAnchorable CanHide=&quot;False&quot; AutoHideMinWidth=&quot;100&quot; AutoHideMinHeight=&quot;100&quot; Title=&quot;Пациенты&quot; IsSelected=&quot;True&quot; ContentId=&quot;Screen&quot; CanClose=&quot;False&quot; CanFloat=&quot;False&quot; LastActivationTimeStamp=&quot;01/08/2015 21:13 [rest of string was truncated]&quot;;.
         /// </summary>
-        public static string avalon_layout {
-            get {
+        public static string avalon_layout
+        {
+            get
+            {
                 return ResourceManager.GetString("avalon_layout", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap Close_16xLG {
-            get {
+        public static System.Drawing.Bitmap Close_16xLG
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("Close_16xLG", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap DeleteHS {
-            get {
+        public static System.Drawing.Bitmap DeleteHS
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("DeleteHS", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap Find_5650 {
-            get {
+        public static System.Drawing.Bitmap Find_5650
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("Find_5650", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap GroupBy {
-            get {
+        public static System.Drawing.Bitmap GroupBy
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("GroupBy", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to в осмотре.
         /// </summary>
-        public static string HealthRecordQueryAndScope_Appointment {
-            get {
+        public static string HealthRecordQueryAndScope_Appointment
+        {
+            get
+            {
                 return ResourceManager.GetString("HealthRecordQueryAndScope_Appointment", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to в курсе.
         /// </summary>
-        public static string HealthRecordQueryAndScope_Course {
-            get {
+        public static string HealthRecordQueryAndScope_Course
+        {
+            get
+            {
                 return ResourceManager.GetString("HealthRecordQueryAndScope_Course", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to в записи.
         /// </summary>
-        public static string HealthRecordQueryAndScope_HealthRecord {
-            get {
+        public static string HealthRecordQueryAndScope_HealthRecord
+        {
+            get
+            {
                 return ResourceManager.GetString("HealthRecordQueryAndScope_HealthRecord", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to у пациента.
         /// </summary>
-        public static string HealthRecordQueryAndScope_Patient {
-            get {
+        public static string HealthRecordQueryAndScope_Patient
+        {
+            get
+            {
                 return ResourceManager.GetString("HealthRecordQueryAndScope_Patient", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap save_16xLG {
-            get {
+        public static System.Drawing.Bitmap save_16xLG
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("save_16xLG", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        public static System.Drawing.Bitmap SortAscending {
-            get {
+        public static System.Drawing.Bitmap SortAscending
+        {
+            get
+            {
                 object obj = ResourceManager.GetObject("SortAscending", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to По категории.
         /// </summary>
-        public static string Sorting_Category {
-            get {
+        public static string Sorting_Category
+        {
+            get
+            {
                 return ResourceManager.GetString("Sorting_Category", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to По дате создания.
         /// </summary>
-        public static string Sorting_CreatedAt {
-            get {
+        public static string Sorting_CreatedAt
+        {
+            get
+            {
                 return ResourceManager.GetString("Sorting_CreatedAt", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to По дате.
         /// </summary>
-        public static string Sorting_Date {
-            get {
+        public static string Sorting_Date
+        {
+            get
+            {
                 return ResourceManager.GetString("Sorting_Date", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to (нет).
         /// </summary>
-        public static string Sorting_None {
-            get {
+        public static string Sorting_None
+        {
+            get
+            {
                 return ResourceManager.GetString("Sorting_None", resourceCulture);
             }
         }
-        
+
         /// <summary>
         ///   Looks up a localized string similar to По порядку.
         /// </summary>
-        public static string Sorting_Ord {
-            get {
+        public static string Sorting_Ord
+        {
+            get
+            {
                 return ResourceManager.GetString("Sorting_Ord", resourceCulture);
             }
         }
