4f6ababb719eb387e6ceac122e802414a498edff
smg <semagerasimov@gmail.com>
2015-03-13 14:31:59 +0700

    xaml. Rename Diagnosis.Client.App.Controls.FormParts namespace

--- Diagnosis.Client.App/Controls/FormParts/ConfirmPasswordControl.xaml.cs
+++ Diagnosis.Client.App/Controls/FormParts/ConfirmPasswordControl.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace Diagnosis.Client.App.Controls.Editors
+namespace Diagnosis.Client.App.Controls.FormParts
 {
     public partial class ConfirmPasswordControl : UserControl
     {
