e4052dc45ad406589ea0c57d6726a1d540f004be
smg <semagerasimov@gmail.com>
2015-03-19 18:10:49 +0700

    ui. Fix ChildWindow OnKeyDown

---
+++ Diagnosis.Client.App/Windows/Shell/MyChildWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xceed.Wpf.Toolkit;
+
+namespace Diagnosis.Client.App.Windows.Shell
+{
+    public class MyChildWindow : ChildWindow
+    {
+        protected override void OnKeyDown(System.Windows.Input.KeyEventArgs e)
+        {
+            // don't Move window
+        }
+    }
+}
