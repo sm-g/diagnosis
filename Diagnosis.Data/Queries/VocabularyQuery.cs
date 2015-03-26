0f37ed0b7186f4991c6ba30e7d3bf17f90a6014a
smg <semagerasimov@gmail.com>
2015-03-26 18:09:37 +0700

    data. Add VocabularyQuery.Custom
    --HG--
    branch : f/Vocabulary

---
+++ Diagnosis.Data/Queries/VocabularyQuery.cs
@@ -0,0 +1,30 @@
+﻿using Diagnosis.Common;
+using Diagnosis.Models;
+using NHibernate;
+using NHibernate.Linq;
+using System;
+using System.Linq;
+
+namespace Diagnosis.Data.Queries
+{
+    public static class VocabularyQuery
+    {
+        /// <summary>
+        /// Возвращает пользовательский словарь.
+        /// </summary>
+        public static Func<Vocabulary> Custom(ISession session)
+        {
+            return () =>
+            {
+                using (var tr = session.BeginTransaction())
+                {
+                    var voc = session.Query<Vocabulary>()
+                        .Where(x => x.Title == Vocabulary.CustomTitle)
+                        .FirstOrDefault() ?? new Vocabulary(Vocabulary.CustomTitle);
+
+                    return voc;
+                }
+            };
+        }
+    }
+}
