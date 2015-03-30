10886790d6772ea0d7f04c6b1e869894dc78ea15
smg <semagerasimov@gmail.com>
2015-03-31 00:37:06 +0700

    build. Remove CopyWorking

--- build-release.cmd
+++ build-release.cmd
@@ -1,4 +1,4 @@
 @echo off
-rem app, build, setup, CopyWorking
-call build.cmd client 1 0 0
-call build.cmd server 1 0 0
+rem app, build, setup
+call build.cmd client 1 0
+call build.cmd server 1 0
--- build.cmd
+++ build.cmd
@@ -1,6 +1,6 @@
 set msbuild=%windir%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe
 REM set targets=%*
 REM if /i "%targets%" == "" set targets=Start
-%msbuild% build.msbuild /p:App="%1" /p:BuildRelease="%2" /p:BuildAndZipSetup="%3" /p:CopyWorking="%4" 
+%msbuild% build.msbuild /p:App="%1" /p:BuildRelease="%2" /p:BuildAndZipSetup="%3"
 rem /verbosity:minimal
 @pause
