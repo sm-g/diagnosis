85b09ccaf3a7884a7f30c9e9f0fb3b12f382bf52
smg <semagerasimov@gmail.com>
2015-04-09 09:44:37 +0700

    test. PersistTest
    --HG--
    branch : f/Vocabulary

--- Diagnosis.Tests/Data/PersistTest.cs
+++ Diagnosis.Tests/Data/PersistTest.cs
@@ -1,4 +1,6 @@
 ﻿using Diagnosis.Common;
+using Diagnosis.Data;
+using Diagnosis.Data.Sync;
 using Diagnosis.Models;
 using Diagnosis.ViewModels;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
@@ -25,6 +27,41 @@ namespace Diagnosis.Tests.Data
                 session.SaveOrUpdate(p);
                 tx.Commit();
             }
+        }
+
+        [TestMethod]
+        public void AddWordSaveVoc()
+        {
+            Load<Vocabulary>();
+            var w = new Word("asdsad");
+            voc[1].AddWord(w);
+
+            Assert.IsTrue(voc[1].Words.Contains(w));
+
+            new Saver(session).Save(voc[1]);
+
+            Assert.IsTrue(!w.IsTransient);
+        }
+
+        [TestMethod]
+        public void RemoveWordSaveVoc()
+        {
+            Load<Vocabulary>();
+
+            var l = new VocLoader(session);
+            l.LoadOrUpdateVocs(voc[1]);
+            var w = voc[1].Words.First();
+
+            voc[1].RemoveWord(w);
+            w.OnDelete();
+            new Saver(session).Delete(w); // или после
+            new Saver(session).Save(voc[1]);
+
+            Assert.IsTrue(!voc[1].Words.Contains(w));
+            Assert.IsTrue(w.Vocabularies.Count() == 0);
+            Assert.IsTrue(!session.QueryOver<Word>().List().Any(x => x.Title == w.Title));
+            Assert.IsTrue(w.IsTransient);
+
         }
     }
 }
