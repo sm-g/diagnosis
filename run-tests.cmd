93cb921f45b021121a6b7a21ed7781ee64ad57ab
smg <semagerasimov@gmail.com>
2015-05-10 10:35:45 +0700

    tests. Add run-tests.cmd

---
+++ run-tests.cmd
@@ -0,0 +1,2 @@
+set mstest="%programfiles(x86)%\Microsoft Visual Studio 12.0\Common7\IDE\CommonExtensions\Microsoft\TestWindow\vstest.console.exe"
+%mstest% Diagnosis.Common.Tests\bin\Debug\Diagnosis.Common.Tests.dll  Diagnosis.Models.Tests\bin\Debug\Diagnosis.Models.Tests.dll  Diagnosis.Data.Tests\bin\Debug\Diagnosis.Data.Tests.dll Diagnosis.ViewModels.Tests\bin\Debug\Diagnosis.ViewModels.Tests.dll /logger:trx
