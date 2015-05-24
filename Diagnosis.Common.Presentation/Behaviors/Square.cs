2f5b11b588ee2fdf76e50f6685e2686b3942a52c
smg <semagerasimov@gmail.com>
2015-05-24 19:01:17 +0700

    xaml. MenuItemIcon, icons for tag convert

---
+++ Diagnosis.Common.Presentation/Behaviors/Square.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+
+namespace Diagnosis.Common.Presentation.Behaviors
+{
+    public class Square
+    {
+        public static readonly DependencyProperty SizeProperty =
+            DependencyProperty.RegisterAttached("Size", typeof(double?), typeof(Square), new PropertyMetadata(null, (sender, e) =>
+              {
+                  if (((double?)e.NewValue).HasValue)
+                  {
+                      var element = sender as FrameworkElement;
+                      if (element == null) return;
+
+                      var size = ((double?)e.NewValue).Value;
+                      element.Width = size;
+                      element.Height = size;
+                  }
+              }));
+
+        public static double? GetSize(DependencyObject obj)
+        {
+            return (double?)obj.GetValue(SizeProperty);
+        }
+
+        public static void SetSize(DependencyObject obj, double? value)
+        {
+            obj.SetValue(SizeProperty, value);
+        }
+    }
+}
---
+++ Diagnosis.Common.Presentation/Controls/Common/MenuItemIcon.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Diagnosis.Common.Presentation.Controls
+{
+    public partial class MenuItemIcon : UserControl
+    {
+        public static readonly DependencyProperty SourceProperty =
+           DependencyProperty.Register("Source", typeof(Visual), typeof(MenuItemIcon));
+
+        public static readonly DependencyProperty FillProperty =
+            DependencyProperty.Register("Fill", typeof(Brush), typeof(MenuItemIcon));
+
+        public MenuItemIcon()
+        {
+            InitializeComponent();
+        }
+        public Visual Source
+        {
+            get { return (Visual)GetValue(SourceProperty); }
+            set { SetValue(SourceProperty, value); }
+        }
+
+        public Brush Fill
+        {
+            get { return (Brush)GetValue(FillProperty); }
+            set { SetValue(FillProperty, value); }
+        }
+    }
+}
